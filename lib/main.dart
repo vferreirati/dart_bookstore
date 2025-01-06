7a877e05c75840464e01e139c7e0b089470bd66f
Victor Ferreira <vferreirati@gmail.com>
2025-01-06 11:50:09 -0300

    feat: add dotenv support for environment variables and update database connection

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:postgres/postgres.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:dotenv/dotenv.dart';
 
 import 'handler/user_handler.dart';
 import 'middlewares/error_handling_middleware.dart';
@@ -13,9 +14,16 @@ import 'repository/user_repository.dart';
 import 'service/user_service.dart';
 
 void main() async {
+  final env = DotEnv()..load();
+
   late Connection connection;
   try {
-    connection = await connectToDatabase();
+    connection = await connectToDatabase(
+      host: env['DATABASE_HOST']!,
+      database: env['DATABASE_NAME']!,
+      username: env['DATABASE_USER']!,
+      password: env['DATABASE_PASSWORD']!,
+    );
   } catch (e) {
     print('Failed to connect to database: $e');
     return;
@@ -28,7 +36,7 @@ void main() async {
     return;
   }
 
-  const secret = 'secret';
+  String secret = env['JWT_SECRET']!;
   final userHandler = UserHandler(UserService(UserRepository(connection)));
 
   final publicRouter = Router();
@@ -60,12 +68,17 @@ void main() async {
   print('Server running on localhost:${server.port}');
 }
 
-Future<Connection> connectToDatabase() async {
+Future<Connection> connectToDatabase({
+  required String host,
+  required String database,
+  required String username,
+  required String password,
+}) async {
   final endpoint = Endpoint(
-    host: 'localhost',
-    database: 'bookstore',
-    username: 'bookstore',
-    password: '123456',
+    host: host,
+    database: database,
+    username: username,
+    password: password,
   );
 
   return Connection.open(
